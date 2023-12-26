601a0b36438ceef69f8f884d52abdef83061e7a9
akaRemeris <george.lalala@yandex.ru>
2023-12-26 17:36:30 +0300

    fix translator RequestError

--- model_api.py
+++ model_api.py
@@ -1,8 +1,45 @@
 "Rough interface for HF summarization model pipeline."
 
+from typing import Callable
 from deep_translator import GoogleTranslator as Translator
+from deep_translator.exceptions import RequestError
+from nltk.tokenize import sent_tokenize
 from langdetect import detect
 from transformers import pipeline
+
+
+def recursive_split(translator: Translator,
+                    sentences: list,
+                    split_idx: int = None) -> str:
+    """
+    If translation ain't successeful due to sequence length,
+    split it in half and try again.
+    """
+    split_idx = len(sentences)//2 if split_idx is None else split_idx//2
+    try:
+        translated_piece = translator.translate(' '.join(sentences))
+        return translated_piece
+    except RequestError:
+        left_sentences = recursive_split(translator,
+                                         sentences[:split_idx],
+                                         split_idx)
+        right_sentences = recursive_split(translator,
+                                          sentences[split_idx:],
+                                          split_idx)
+        return left_sentences + right_sentences
+
+def split_translation(translator: Translator,
+                      text: str,
+                      splitter_foo: Callable = recursive_split,
+                      **splitter_kwargs: dict) -> str:
+    """
+    Preprocess input and call the optional splitter.
+    """
+    sentence_splited_text = sent_tokenize(text)
+    translated_text = splitter_foo(translator,
+                                   sentence_splited_text,
+                                   **splitter_kwargs)
+    return translated_text
 
 class ModelAPI():
     """
@@ -67,13 +104,15 @@ class ModelAPI():
         if self.input_language == self.output_language == 'auto':
             self.summary_translator = Translator(source=self.model_language,
                                                  target=detect(context))
-        return self.context_translator.translate(context)
+        translated_context = split_translation(self.context_translator, context)
+        return translated_context
 
     def _check_output_language(self, summary: str) -> str:
         """
         Translate output summary.
         """
-        return self.summary_translator.translate(summary)
+        translated_summary = split_translation(self.summary_translator, summary)
+        return translated_summary
 
     def _get_model_output(self, context: str) -> str:
         """
